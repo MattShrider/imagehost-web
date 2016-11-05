e10820f4289ad3e796f65e3935763bcb6660da84
Matthew Shrider <mattshrider@gmail.com>
2016-11-05 12:53:37 -0400

    Initial code and webpack

---
+++ config/webpack.common.js
@@ -0,0 +1,54 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+
+var helpers = require('./helpers');
+
+module.exports = {
+  entry: {
+    'app': './index.ts',
+    'vendor': './vendor.ts',
+    'polyfills': './polyfills.ts'
+  },
+
+  resolve: {
+    extensions: ['', '.ts', '.js'],
+    root: path.resolve('..')
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      },
+      {
+        test: /\.(png|pje?g|gif|ssvg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.css$/,
+        exclude: helpers.root('app'),
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+      },
+      {
+        test: /\.css$/,
+        include: helpers.root('app')
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
+};
